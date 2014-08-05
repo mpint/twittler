cfe590852fc098faa77458fc2acd2de52b137598
Michael <mpinter09@gmail.com>
2014-08-05 00:54:21 -0700

    'added comments to data_generator.js'

--- data_generator.js
+++ data_generator.js
@@ -4,24 +4,24 @@
  */
 
 // set up data structures
-window.streams = {};
-streams.home = [];
-streams.users = {};
-streams.users.shawndrost = [];
-streams.users.sharksforcheap = [];
-streams.users.mracus = [];
-streams.users.douglascalhoun = [];
-window.users = Object.keys(streams.users);
+window.streams = {}; // streams property of window
+streams.home = []; // home property of streams
+streams.users = {}; // users property of streams
+streams.users.shawndrost = []; // username array in users property
+streams.users.sharksforcheap = []; // username array in users property
+streams.users.mracus = []; // username array in users property
+streams.users.douglascalhoun = []; // username array in users property
+window.users = Object.keys(streams.users); // populates users property of windows with array of usernames [shawndrost,sharksforcheap,...]
 
 // utility function for adding tweets to our data structures
-var addTweet = function(newTweet){
-  var username = newTweet.user;
-  streams.users[username].push(newTweet);
-  streams.home.push(newTweet);
+var addTweet = function(newTweet){ // newTweet is a string comprised of a tweet
+  var username = newTweet.user; // gets username from user property of tweet in generateRandomTweet
+  streams.users[username].push(newTweet); // pushes newTweet to username array
+  streams.home.push(newTweet); // pushes newTweet to home property array
 };
 
 // utility function
-var randomElement = function(array){
+var randomElement = function(array){ // returns random element of array
   var randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 };
@@ -33,17 +33,17 @@ var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the
 var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
 var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
 
-var randomMessage = function(){
+var randomMessage = function(){ // generates ordered array of a random tweet
   return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
 };
 
 // generate random tweets on a random schedule
 var generateRandomTweet = function(){
   var tweet = {};
-  tweet.user = randomElement(users);
-  tweet.message = randomMessage();
-  tweet.created_at = new Date();
-  addTweet(tweet);
+  tweet.user = randomElement(users); // assigns a random user for user property of tweet
+  tweet.message = randomMessage(); // generates random tweet for message property of tweet
+  tweet.created_at = new Date(); // generates current timestamp for tweet
+  addTweet(tweet); // inintializes addTweet inside the scope of generateRandomTweet
 };
 
 for(var i = 0; i < 10; i++){
@@ -52,7 +52,7 @@ for(var i = 0; i < 10; i++){
 
 var scheduleNextTweet = function(){
   generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math.random() * 1500);
+  setTimeout(scheduleNextTweet, Math.random() * 1500); // generates random tweet every 1.5s or less
 };
 scheduleNextTweet();
 
